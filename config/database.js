bc1c07f428a9ac5507208db8dd652c8edac9587c
mahaamad787-commits <mahaamad787@gmail.com>
2026-09-12 18:01:01 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,1 +1,25 @@
+const mongoose = require("mongoose");
 
+async function connectDatabase() {
+  try {
+    const mongoURI = process.env.MONGODB_URI;
+
+    if (!mongoURI) {
+      throw new Error("MONGODB_URI غير موجود في ملف .env");
+    }
+
+    await mongoose.connect(mongoURI);
+
+    console.log("=================================");
+    console.log("      DATABASE CONNECTED");
+    console.log("      VOLTARENA DATABASE");
+    console.log("=================================");
+  } catch (error) {
+    console.error("Database connection failed:");
+    console.error(error.message);
+
+    process.exit(1);
+  }
+}
+
+module.exports = connectDatabase;
